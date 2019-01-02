5f58234f976da9430531bbeb91929c0bc27ef6ef
袁常鑫 <729056559@qq.com>
2019-01-02 13:47:32 +0800

    初始化项目

---
+++ somnous-parent/common-redis/src/main/java/somnous/y/commonredis/CommonRedisApplication.java
@@ -0,0 +1,14 @@
+package somnous.y.commonredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonRedisApplication.class, args);
+	}
+
+}
+
