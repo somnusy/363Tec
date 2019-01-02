b780fe208fdeff6b3b718bef040ea7de24693993
袁常鑫 <729056559@qq.com>
2019-01-02 17:25:29 +0800

    eureka  admin初始化

--- somnous-parent/admin-service/src/main/java/somnous/y/adminservice/AdminServiceApplication.java
+++ somnous-parent/admin-service/src/main/java/somnous/y/adminservice/AdminServiceApplication.java
@@ -1,14 +1,17 @@
 package somnous.y.adminservice;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServiceApplication.class, args);
 	}
-
 }
 
