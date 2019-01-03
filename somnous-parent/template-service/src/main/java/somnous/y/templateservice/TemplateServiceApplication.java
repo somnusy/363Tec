c9a2ff761f87a637e0a1ce4a8eb9b0034db8421e
袁常鑫 <729056559@qq.com>
2019-01-03 16:38:48 +0800

    初始化template-service

---
+++ somnous-parent/template-service/src/main/java/somnous/y/templateservice/TemplateServiceApplication.java
@@ -0,0 +1,14 @@
+package somnous.y.templateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateServiceApplication.class, args);
+	}
+
+}
+
